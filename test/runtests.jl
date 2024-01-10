981334dc6dbe24ef3545f36bd9b9b78e12aef41e
Cheng Gong <enigne@gmail.com>
2024-01-09 20:18:33 -0500

    remove all test use ISSMDIR()

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,16 +9,16 @@ end
 	@time @testset "Model Struct Tests" begin include("modelstructtests.jl") end
 
 	# test each individual cases, name with test[0-9]*.jl
-	testsolutions = searchdir("./", r"test[0-9]*.jl")
-	@time @testset "Model Solution Tests" begin
-		for tf in testsolutions
-			include(tf)
-		end
-	end
+	#testsolutions = searchdir("./", r"test[0-9]*.jl")
+	#@time @testset "Model Solution Tests" begin
+	#	for tf in testsolutions
+	#		include(tf)
+	#	end
+	#end
 
 	# AD test
 	@time include("testad.jl")
-	@time include("testad2.jl")
+	#@time include("testad2.jl")
 
 	# GPU test
 	#@time include("testGPU.jl")
