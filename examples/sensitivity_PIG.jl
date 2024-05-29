d14031706337a7f4bad84d41a3327bfa0fcdab9d
Cheng Gong <enigne@gmail.com>
2024-05-29 14:14:18 -0400

    add a sensitivity example of Pine Island Glacier

---
+++ examples/sensitivity_PIG.jl
@@ -0,0 +1,26 @@
+using Enzyme
+Enzyme.API.typeWarning!(false)
+Enzyme.Compiler.RunAttributor[] = false
+
+using DJUICE
+using MAT
+
+#Load model from MATLAB file
+file = matopen(joinpath(@__DIR__, "..", "Data","PIG_Control_drag_dJUICE.mat"))
+
+mat  = read(file, "md")
+close(file)
+md = model(mat)
+
+#make model run faster 
+md.stressbalance.maxiter = 20
+
+#Now call AD!
+md.inversion.iscontrol = 1
+md.inversion.independent = md.friction.coefficient
+md.inversion.independent_string = "FrictionCoefficient"
+
+md = solve(md, :grad)
+
+# the gradient
+g = md.results["StressbalanceSolution"]["Gradient"]
